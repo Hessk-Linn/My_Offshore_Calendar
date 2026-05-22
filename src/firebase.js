c66ba3b58605c34ca7dd0621bb3cff5a5a755d40
Hessk-Linn <hessklinn@gmail.com>
2026-05-22 20:20:00 +0630

    Updated Firebase config with exact pasted credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@ import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager
 
 // Your web app's Firebase configuration. Replace these with your actual keys from Firebase Console.
 const firebaseConfig = {
-  apiKey: "AIzaSyDs5tvsZUUu0pZuo_vhbFl-YFkaOCIp3NA",
+  apiKey: "AIzaSyDs5tvsZUUu0pZuo_vhbFl-YFkAoCIp3NA",
   authDomain: "myoffshorerotation.firebaseapp.com",
   projectId: "myoffshorerotation",
-  storageBucket: "myoffshorerotation.appspot.com",
+  storageBucket: "myoffshorerotation.firebasestorage.app",
   messagingSenderId: "544983536880",
   appId: "1:544983536880:web:009e1f8c8b86dea6265e35"
 };
